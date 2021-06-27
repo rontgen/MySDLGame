ebe82fbe446010317393a7a33e39c7515f7fe586
gwang <minstrelgen@163.com>
2021-06-27 12:28:50 +0800

    SDL Game bare bones

--- main.cpp
+++ main.cpp
@@ -2,25 +2,64 @@
 
 SDL_Window* g_pWindow = 0;
 SDL_Renderer* g_pRenderer = 0;
+bool g_bRunning = false;
+
+void init();
+bool init(const char* title, int xpos, int ypos, int w, int h, int flags);
+void render(Uint8 r, Uint8 g, Uint8 b, Uint8 a);
+void update();
+void handleEvent();
+void clean();
 
 int main(int argc, char* argv[])
 {
-    if (SDL_Init(SDL_INIT_EVERYTHING) >=0)
+    if (init("My SDL Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640,480, SDL_WINDOW_SHOWN))
     {
-        g_pWindow = SDL_CreateWindow("My SDL Game framework", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1024, 640, SDL_WINDOW_SHOWN);
-        if (g_pWindow != 0)
-        {
-            g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
-        }
+        g_bRunning = true;
     }
     else
     {
         return 1;
     }
-    SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 0, 255);
-    SDL_RenderClear(g_pRenderer);
-    SDL_RenderPresent(g_pRenderer);
-    SDL_Delay(5000);
+    while (g_bRunning)
+    {
+        render(0, 0, 0, 255);
+    }
     SDL_Quit();
     return 0;
+}
+
+
+bool init(const char* title, int xpos, int ypos, int w, int h, int flags)
+{
+    if (SDL_Init(SDL_INIT_EVERYTHING) >=0)
+    {
+        g_pWindow = SDL_CreateWindow(title, xpos, ypos, w, h, flags);
+        if (g_pWindow!=0)
+        {
+            g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
+        }
+    } 
+    else
+    {
+        return false;
+    }
+    return true;
+}
+
+void render(Uint8 r, Uint8 g, Uint8 b, Uint8 a)
+{
+    SDL_SetRenderDrawColor(g_pRenderer, r, g, b, a);
+    SDL_RenderClear(g_pRenderer);
+    SDL_RenderPresent(g_pRenderer);
+}
+
+void update()
+{}
+
+void handleEvent()
+{}
+
+void clean()
+{
 }
